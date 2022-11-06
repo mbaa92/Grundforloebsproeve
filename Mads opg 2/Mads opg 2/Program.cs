5a71d98f818e8c723db7cd243cb051f4461d54b7
mbaa92 <117586169+mbaa92@users.noreply.github.com>
2022-11-06 16:52:45 +0100

    Add files via upload

---
+++ Mads opg 2/Mads opg 2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mads_opg_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Mads opgave 2
+
+            string citat;
+            bool check = true; //til at stoppe loopet
+
+            do
+            {
+                Console.WriteLine("Indtast et citat, der lever op til følgende krav");
+                Console.WriteLine("1) Citatet skal min. indeholde 20 tegn og maks. indeholde 50 bogstaver");
+                Console.WriteLine("2) Citatet skal enten starte med bogstavet 'D' eller ende på bogstave 'T'");
+                citat=Console.ReadLine().ToUpper();
+
+                if (citat.Length >= 20 && citat.Count(char.IsLetter) <= 50) //tester første krav
+                {
+                    if (citat.StartsWith("D") && !citat.EndsWith("T")) //tester andet krav
+                    {
+                        Console.WriteLine("Det lykkedes, tryk på en tast for at afslutte");
+                        check = false;
+                        Console.ReadKey();
+                    }
+                    else if (!citat.StartsWith("D") && citat.EndsWith("T")) //tester andet krav
+                    {
+                        Console.WriteLine("Det lykkedes, tryk på en tast for at aflutte");
+
+                        check = false;
+                        Console.ReadKey();
+                    }
+
+                }
+                Console.Clear();
+            } while (check);
+            
+        }
+    }
+}
